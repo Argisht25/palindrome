1ce068e85c5898eb82e530c94aac76cd29273b96
Argisht <100313895+Argisht25@users.noreply.github.com>
2022-05-29 00:10:00 +0400

    Update newJS.js

--- newJS.js
+++ newJS.js
@@ -1,9 +1,7 @@
 function palindrome(str) {
     let check = '';
     for (let i = str.length - 1; i >= 0; --i) {
-      check += str[i];
+         check += str[i];
     }
     return str == check;
   }
-  
-  console.log(palindrome('zzxzz'));
